dc1c5971f13e1abffd92b457dc729430a6ba3664
Jorge <Jorgecastruita1406@gmail.com>
2026-01-30 15:50:53 -0600

    Se agrego 2 campos de texto y 2 titutlos donde se registra un dato en cada uno y con el boton agregado se Calcula la suma de los valores (Solo Valores Enteros) y por Ultimo se Agrega un boton "Reset" que borra los datos en los cuadros de texto y los ponen en nulos

---
+++ Calculadora/frmCalculadora.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Calculadora
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblVariableA = new Label();
+            lblVariableB = new Label();
+            txtVariableA = new TextBox();
+            txtVariableB = new TextBox();
+            btnCalcular = new Button();
+            btnReset = new Button();
+            SuspendLayout();
+            // 
+            // lblVariableA
+            // 
+            lblVariableA.AutoSize = true;
+            lblVariableA.Location = new Point(11, 28);
+            lblVariableA.Name = "lblVariableA";
+            lblVariableA.Size = new Size(59, 15);
+            lblVariableA.TabIndex = 0;
+            lblVariableA.Text = "Vairbale A";
+            // 
+            // lblVariableB
+            // 
+            lblVariableB.AutoSize = true;
+            lblVariableB.Location = new Point(12, 102);
+            lblVariableB.Name = "lblVariableB";
+            lblVariableB.Size = new Size(58, 15);
+            lblVariableB.TabIndex = 1;
+            lblVariableB.Text = "Variable B";
+            // 
+            // txtVariableA
+            // 
+            txtVariableA.Location = new Point(11, 46);
+            txtVariableA.Name = "txtVariableA";
+            txtVariableA.Size = new Size(120, 23);
+            txtVariableA.TabIndex = 2;
+            // 
+            // txtVariableB
+            // 
+            txtVariableB.Location = new Point(11, 120);
+            txtVariableB.Name = "txtVariableB";
+            txtVariableB.Size = new Size(120, 23);
+            txtVariableB.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            btnCalcular.Location = new Point(186, 63);
+            btnCalcular.Name = "btnCalcular";
+            btnCalcular.Size = new Size(75, 23);
+            btnCalcular.TabIndex = 4;
+            btnCalcular.Text = "Calcular";
+            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.Click += btnCalcular_Click;
+            // 
+            // btnReset
+            // 
+            btnReset.Location = new Point(186, 102);
+            btnReset.Name = "btnReset";
+            btnReset.Size = new Size(75, 23);
+            btnReset.TabIndex = 5;
+            btnReset.Text = "Reset";
+            btnReset.UseVisualStyleBackColor = true;
+            btnReset.Click += btnReset_Click;
+            // 
+            // frmCalculadora
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(340, 168);
+            Controls.Add(btnReset);
+            Controls.Add(btnCalcular);
+            Controls.Add(txtVariableB);
+            Controls.Add(txtVariableA);
+            Controls.Add(lblVariableB);
+            Controls.Add(lblVariableA);
+            Name = "frmCalculadora";
+            Text = "Calculadora";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblVariableA;
+        private Label lblVariableB;
+        private TextBox txtVariableA;
+        private TextBox txtVariableB;
+        private Button btnCalcular;
+        private Button btnReset;
+    }
+}
---
+++ Calculadora/frmCalculadora.cs
@@ -0,0 +1,47 @@
+namespace Calculadora
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                int a = 0, b = 0, resultado;
+                a = int.Parse(txtVariableA.Text);
+                b = Convert.ToInt32(txtVariableB.Text);
+
+                resultado = a + b;
+                MessageBox.Show("El Resultado es: " + resultado.ToString(),
+                    "Sistema", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Reset();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("Error en la conversión de datos",
+                    "Sistema", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Reset();
+
+            }
+        }
+
+        private void Reset()
+        {
+            txtVariableA.Text = "";
+            txtVariableB.Clear();
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+    }
+}

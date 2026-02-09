ea9f8d973f853e808771df51d6b7fa9a96d997a1
Jorge <Jorgecastruita1406@gmail.com>
2026-02-09 15:47:21 -0600

    Se agrega alarma con sonido de gallo y un mensaje para preguntar a que hr quieres que suene tu alarma

--- Calculadora/Formularios/frmTemporizadores.Designer.cs
+++ Calculadora/Formularios/frmTemporizadores.Designer.cs
@@ -31,6 +31,10 @@
             components = new System.ComponentModel.Container();
             lblHora = new Label();
             tmrHora = new System.Windows.Forms.Timer(components);
+            mnsAlarma = new MenuStrip();
+            establecerAlarmaToolStripMenuItem = new ToolStripMenuItem();
+            alarma1ToolStripMenuItem = new ToolStripMenuItem();
+            mnsAlarma.SuspendLayout();
             SuspendLayout();
             // 
             // lblHora
@@ -49,14 +53,41 @@
             tmrHora.Interval = 1000;
             tmrHora.Tick += tmrHora_Tick;
             // 
+            // mnsAlarma
+            // 
+            mnsAlarma.Items.AddRange(new ToolStripItem[] { establecerAlarmaToolStripMenuItem });
+            mnsAlarma.Location = new Point(0, 0);
+            mnsAlarma.Name = "mnsAlarma";
+            mnsAlarma.Size = new Size(414, 24);
+            mnsAlarma.TabIndex = 1;
+            mnsAlarma.Text = "menuStrip1";
+            // 
+            // establecerAlarmaToolStripMenuItem
+            // 
+            establecerAlarmaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { alarma1ToolStripMenuItem });
+            establecerAlarmaToolStripMenuItem.Name = "establecerAlarmaToolStripMenuItem";
+            establecerAlarmaToolStripMenuItem.Size = new Size(75, 20);
+            establecerAlarmaToolStripMenuItem.Text = "Establecer ";
+            // 
+            // alarma1ToolStripMenuItem
+            // 
+            alarma1ToolStripMenuItem.Name = "alarma1ToolStripMenuItem";
+            alarma1ToolStripMenuItem.Size = new Size(180, 22);
+            alarma1ToolStripMenuItem.Text = "Alarma 1";
+            alarma1ToolStripMenuItem.Click += alarma1ToolStripMenuItem_Click;
+            // 
             // frmTemporizadores
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(414, 133);
             Controls.Add(lblHora);
+            Controls.Add(mnsAlarma);
+            MainMenuStrip = mnsAlarma;
             Name = "frmTemporizadores";
             Text = "Reloj";
+            mnsAlarma.ResumeLayout(false);
+            mnsAlarma.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -65,5 +96,8 @@
 
         private Label lblHora;
         private System.Windows.Forms.Timer tmrHora;
+        private MenuStrip mnsAlarma;
+        private ToolStripMenuItem establecerAlarmaToolStripMenuItem;
+        private ToolStripMenuItem alarma1ToolStripMenuItem;
     }
 }
--- Calculadora/Formularios/frmTemporizadores.cs
+++ Calculadora/Formularios/frmTemporizadores.cs
@@ -5,11 +5,16 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.Media;
+using MediaPlayer;
+using CSCore.SoundOut;
 
 namespace Calculadora.Formularios
 {
     public partial class frmTemporizadores : Form
     {
+        string alarma1 = "";
         public frmTemporizadores()
         {
             InitializeComponent();
@@ -17,7 +22,19 @@ namespace Calculadora.Formularios
 
         private void tmrHora_Tick(object sender, EventArgs e)
         {
+           
             lblHora.Text = DateTime.Now.ToLongTimeString();
+          
+            if (lblHora.Text == alarma1)
+            {
+               SoundPlayer player = new SoundPlayer("C:\\Users\\jorge\\source\\repos\\Calculadora\\Calculadora\\Sonidos\\dragon-studio-rooster-call-364474.wav");
+                player.Play();
+            }
+        }
+
+        private void alarma1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            alarma1 = Interaction.InputBox("Ingresa la Hora", "Sistema","00:00:00 x.x");
         }
     }
 }
